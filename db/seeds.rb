723c0565b8ab27d1a519a207ce14feb519d57320
mrandolph83 <mrandolph83@gmail.com>
2021-07-09 00:12:49 -0400

    Ported over seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+p1 = Employee.create([{ name: 'Mario', bio: 'Plumber guy, loves wearing Red and rescuing Princess Peach', headshot: 'https://cdn.vox-cdn.com/thumbor/Y4WKd5g-roKr1_lO-cUT-yTG3fM=/0x0:1280x720/1200x800/filters:focal(492x239:696x443)/cdn.vox-cdn.com/uploads/chorus_image/image/69054680/nintendo_direct_mario.0.0.0.0.jpg'}, 
+{name: 'Luigi', bio: 'Plumber guy, loves wearing green, and chasing ghosts', headshot: 'https://assets2.ignimgs.com/2014/08/29/newsuperluigiwii1280jpg-ed620a_160w.jpg?width=1280' }])
+
