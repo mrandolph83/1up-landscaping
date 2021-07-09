d85a68ca093e7210aa03177953ad81827fed15e8
mrandolph83 <mrandolph83@gmail.com>
2021-07-09 00:06:36 -0400

    Ported over routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  get '/' => 'sessions#welcome'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+  delete '/logout' => 'sessions#destroy'
   resources :reviews
-  resources :jobs
+  resources :jobs do
+    resources :reviews, only: [:new, :index]
+    end
   resources :employees
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
