3bd748ee26c81bd931016f1a3c2102e62e38177c
mrandolph83 <mrandolph83@gmail.com>
2021-07-10 22:38:21 -0400

    Job service checkboxes working

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,6 +3,7 @@ class Job < ApplicationRecord
     belongs_to :employee
     has_many :reviews
     has_many_attached :images
+    serialize :services
 
     accepts_nested_attributes_for :employee
 end
