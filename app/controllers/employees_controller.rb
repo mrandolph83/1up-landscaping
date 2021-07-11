19561eb002f7d449280903ae779c8be99d1e3af6
mrandolph83 <mrandolph83@gmail.com>
2021-07-11 10:49:32 -0400

    Employee show, index page complete

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,9 +1,13 @@
 class EmployeesController < ApplicationController
 
-    def new
+    def index
+        @employees = Employee.all
     end 
 
-    def create
+    def show
+        @employee = Employee.find_by_id(params[:id])
+      
+        redirect_to '/' if !@employee
     end 
     
 # Add display alphabetical for index
